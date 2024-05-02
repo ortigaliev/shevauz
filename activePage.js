0ed6fc1494cccd2fdb7d06b36e889a29dc43266f
ortigaliev <ortigaliyevm@gmail.com>
2024-05-02 12:36:30 +0900

    feat: Sheva app

---
+++ activePage.js
@@ -0,0 +1,6 @@
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll("nav li a").forEach((link) => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add("active");
+  }
+});
